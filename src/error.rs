5df06895c4e0f7d72ff19e5a82c3b73d84b6a6a3
Maarten de Vries <maarten@de-vri.es>
2020-11-07 23:09:04 +0100

    Add index file parsing.

--- src/error.rs
+++ src/error.rs
@@ -1,26 +1,99 @@
+use std::path::PathBuf;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-#[error("{0}")]
 pub enum InitError {
+	#[error("failed to initialize git repository: {0}")]
 	GitInit(git2::Error),
-	WriteConfig(std::io::Error),
+	#[error("{0}")]
+	WriteFailed(#[from] WriteFailed),
 }
 
 #[derive(Debug, Error)]
-#[error("{0}")]
 pub enum OpenError {
+	#[error("failed to read git repository: {0}")]
 	GitOpen(git2::Error),
 }
 
 #[derive(Debug, Error)]
-#[error("{0}")]
 pub enum AddCrateError {
+	#[error("{0}")]
+	InvalidFileName(#[from] InvalidCrateFileName),
+	#[error("{0}")]
+	DuplicateCrate(#[from] DuplicateCrateVersion),
+	#[error("{0}")]
+	ReadIndex(#[from] ReadIndexError),
+	#[error("{0}")]
+	ReadFailed(#[from] ReadFailed),
+	#[error("failed to commit changes: {0}")]
 	CommitFailed(git2::Error),
+}
+
+#[derive(Debug, Error)]
+pub enum ReadIndexError {
+	#[error("{0}")]
+	ReadFailed(#[from] ReadFailed),
+	#[error("invalid UTF-8 in index: {0}")]
+	InvalidUtf8(#[from] InvalidUt8File),
+}
+
+impl ReadIndexError {
+	/// Check if reading the index failed because the index file does not exist.
+	pub fn is_not_found(&self) -> bool {
+		if let Self::ReadFailed(e) = self {
+			e.error.kind() == std::io::ErrorKind::NotFound
+		} else {
+			false
+		}
+	}
+}
+
+#[derive(Debug, Error)]
+#[error("invalid file name for packaged crate: expected $name-$version.crate")]
+pub struct InvalidCrateFileName {
+	pub path: PathBuf,
+}
+
+#[derive(Debug, Error)]
+#[error("duplicate crate: {name}-{version} already exists in registry")]
+pub struct DuplicateCrateVersion {
+	pub name: String,
+	pub version: String,
 }
 
 #[derive(Debug, Error)]
 #[error("{0}")]
 pub enum YankCrateError {
+	#[error("failed to commit changes: {0}")]
 	CommitFailed(git2::Error),
+}
+
+#[derive(Debug, Error)]
+#[error("failed to read from {path}: {error}")]
+pub struct ReadFailed {
+	/// The file that failed to read.
+	pub path: PathBuf,
+
+	/// The I/O error that occured.
+	pub error: std::io::Error,
+}
+
+#[derive(Debug, Error)]
+#[error("failed to read {path}: {error}")]
+pub struct InvalidUt8File {
+	/// The file that failed to read.
+	pub path: PathBuf,
+
+	/// The I/O error that occured.
+	pub error: std::str::Utf8Error,
+}
+
+#[derive(Debug, Error)]
+#[error("failed to write to {path}: {error}")]
+pub struct WriteFailed {
+	/// The file that failed to read.
+	pub path: PathBuf,
+
+	/// The I/O error that occured.
+	pub error: std::io::Error,
 }
--- src/registry.rs
+++ src/registry.rs
@@ -1,5 +1,5 @@
 use crate::{error, Config};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub struct Registry {
 	repo: git2::Repository,
@@ -12,10 +12,10 @@ impl Registry {
 		let repo = git2::Repository::init(path)
 			.map_err(error::InitError::GitInit)?;
 
-		write_new(
+		write_new_file(
 			path.join("Palletizer.toml"),
 			&toml::ser::to_vec(config).unwrap(),
-		).map_err(error::InitError::WriteConfig)?;
+		)?;
 
 		Ok(Self {
 			repo
@@ -35,22 +35,125 @@ impl Registry {
 	}
 
 	/// Add a crate to the registry.
-	pub fn add_crate(&mut self, path: impl AsRef<Path>) -> Result<Self, error::AddCrateError> {
+	///
+	/// You must pass the path to a crate as packaged by `cargo package`.
+	pub fn add_crate(&mut self, path: impl AsRef<Path>) -> Result<(), error::AddCrateError> {
+		let path = path.as_ref();
+		let (name, version) = parse_crate_name(path)?;
+
+		// Read the index to find existing versions of the crate.
+		let index = match self.read_index(name) {
+			Ok(x) => x,
+			Err(e) => if e.is_not_found() {
+				Vec::new()
+			} else {
+				return Err(e.into());
+			}
+		};
+
+		// Check that the version isn't in the index yet.
+		if index.iter().find(|x| x == &version).is_some() {
+			return Err(error::DuplicateCrateVersion {
+				name: name.to_string(),
+				version: version.to_string(),
+			}.into());
+		}
+
+		// TODO: Copy the crate file to the download location,
+		// add an entry to the index file.
+		// If not, add it and commit.
 		todo!();
 	}
 
+
 	/// Yank a crate from the registry.
-	pub fn yank_crate(&mut self, name: &str, version: &str) -> Result<Self, error::YankCrateError> {
+	pub fn yank_crate(&mut self, name: &str, version: &str) -> Result<(), error::YankCrateError> {
 		todo!()
+	}
+
+	pub fn read_index(&self, name: &str) -> Result<Vec<String>, error::ReadIndexError> {
+		let path = self.index_path(name);
+		let data = read_file(&path)?;
+		let data = String::from_utf8(data).map_err(|e| error::InvalidUt8File {
+			path,
+			error: e.utf8_error(),
+		})?;
+		Ok(data.lines().map(String::from).collect())
+	}
+
+	fn index_path(&self, name: &str) -> PathBuf {
+		let mut file = match name.as_bytes() {
+			&[] => panic!("empty crate names are not supported"),
+			&[a] => format!("1/{}/{}", a as char, name),
+			&[a, b] => format!("2/{}/{}/{}", a as char, b as char, name),
+			&[a, b, c] => format!("3/{}/{}/{}/{}", a as char, b as char, c as char, name),
+			&[a, b, c, d, ..] => format!("{}{}/{}{}/{}", a as char, b as char, c as char, d as char, name),
+		};
+		file.make_ascii_lowercase();
+
+		self.repo.path().join(file)
 	}
 }
 
-fn write_new(path: impl AsRef<Path>, data: &[u8]) -> std::io::Result<()> {
+fn write_new_file(path: impl AsRef<Path>, data: &[u8]) -> Result<(), error::WriteFailed> {
 	use std::io::Write;
+	let path = path.as_ref();
+
+	let map_err = |error| error::WriteFailed {
+		error,
+		path: path.into(),
+	};
 
 	std::fs::OpenOptions::new()
 		.write(true)
 		.create_new(true)
-		.open(path)?
+		.open(path)
+		.map_err(map_err)?
 		.write_all(&data)
+		.map_err(map_err)
+}
+
+fn read_file(path: impl AsRef<Path>) -> Result<Vec<u8>, error::ReadFailed> {
+	let path = path.as_ref();
+	std::fs::read(path)
+		.map_err(|error| error::ReadFailed {
+			error,
+			path: path.into(),
+		})
+}
+
+fn overwrite_file(path: impl AsRef<Path>, data: &[u8]) -> Result<(), error::WriteFailed> {
+	let path = path.as_ref();
+	std::fs::write(path, data)
+		.map_err(|error| error::WriteFailed {
+			error,
+			path: path.into(),
+		})
+}
+
+fn parse_crate_name(path: &Path) -> Result<(&str, &str), error::InvalidCrateFileName> {
+	let make_err = || error::InvalidCrateFileName {
+		path: path.into(),
+	};
+
+	path
+		.file_name()
+		.ok_or_else(make_err)?
+		.to_str()
+		.ok_or_else(make_err)?
+		.strip_suffix(".crate")
+		.ok_or_else(make_err)?
+		.rpartition('-')
+		.ok_or_else(make_err)
+}
+
+trait Rpartition {
+	fn rpartition(&self, split: char) -> Option<(&Self, &Self)>;
+}
+
+impl Rpartition for str {
+	fn rpartition(&self, split: char) -> Option<(&str, &str)> {
+		let mut parts = self.rsplitn(2, split);
+		Some((parts.next().unwrap(), parts.next()?))
+	}
 }
